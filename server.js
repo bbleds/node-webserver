1c10664b71daacef88ea0302100ff246df626d0f
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-02 11:10:43 -0600

    adding routes via express

--- server.js
+++ server.js
@@ -1,33 +1,55 @@
 "use strict";
 
-const http = require("http");
+const express = require("express");
+const app = express();
 const { PORT } = process.env;
 
-http.createServer((req,res) => {
+// http.createServer((req,res) => {
 
-  if(req.url === "/hello") {
+//   if(req.url === "/hello") {
 
-    const msg = '<h1>Hello World</h1>'
-    res.writeHead(200, {"Content-Type": "text/html"});
+//     const msg = '<h1>Hello World</h1>'
+//     res.writeHead(200, {"Content-Type": "text/html"});
 
-    msg.split("").forEach((char, i)=>{
+//     msg.split("").forEach((char, i)=>{
 
-      setTimeout(()=>{
-         res.write(char);
-      }, 100*i)
-    });
+//       setTimeout(()=>{
+//          res.write(char);
+//       }, 100*i)
+//     });
 
 
 
-  } else if (req.url === "/random"){
-     res.end("<h1>This is so random!!</h1>");
-  } else {
-    res.writeHead(403);
-    res.end("Access Denied!!!")
-  }
+//   } else if (req.url === "/random"){
+//      res.end("<h1>This is so random!!</h1>");
+//   } else {
+//     res.writeHead(403);
+//     res.end("Access Denied!!!")
+//   }
+// });
 
-console.log(req.method, req.url)
-}).listen(PORT, ()=> console.log(`server listening on port ${PORT}, ya filthy animal`));
+//random route
+app.get("/random", (req, res)=>{
+    res.end("RANNNNDOMMMMMMMMMMMMMMM");
+});
+
+//hello route
+app.get("/hello", (req, res)=>{
+    res.end("Hello worlt");
+});
+
+//default route
+app.get("/", (req, res)=>{
+    res.end("hey hey hey hey");
+});
+
+//catch all route
+app.all("*", (req, res)=>{
+    res.writeHead(404);
+    res.end("Page not fount");
+});
+
+app.listen(PORT, ()=> console.log(`server listening on port ${PORT}, ya filthy animal`));
 
 
 
