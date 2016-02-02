ac6925fc358509bc240e4dc50145b14ae99c1098
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-02 12:00:46 -0600

    adding route params and query params

--- server.js
+++ server.js
@@ -4,38 +4,25 @@ const express = require("express");
 const app = express();
 const { PORT } = process.env;
 
-// http.createServer((req,res) => {
-
-//   if(req.url === "/hello") {
-
-//     const msg = '<h1>Hello World</h1>'
-//     res.writeHead(200, {"Content-Type": "text/html"});
-
-//     msg.split("").forEach((char, i)=>{
-
-//       setTimeout(()=>{
-//          res.write(char);
-//       }, 100*i)
-//     });
-
-
-
-//   } else if (req.url === "/random"){
-//      res.end("<h1>This is so random!!</h1>");
-//   } else {
-//     res.writeHead(403);
-//     res.end("Access Denied!!!")
-//   }
-// });
 
 //random route
 app.get("/random", (req, res)=>{
-    res.end("RANNNNDOMMMMMMMMMMMMMMM");
+    res.end("this is the random route");
 });
 
-//hello route
+//hello route with params
 app.get("/hello", (req, res)=>{
-    res.end("Hello worlt");
+
+    res.end(`Hello ${req.query.name}`);
+});
+
+//spaz route with Route params
+app.get("/spaz/:numOne/:numTwo", (req, res)=>{
+  const getRandomArbitrary = (min, max) => {
+    return Math.random() * (max - min) + min;
+  }
+
+    res.end(`${getRandomArbitrary(parseInt(req.params.numOne),parseInt(req.params.numTwo))}`);
 });
 
 //default route
@@ -45,8 +32,11 @@ app.get("/", (req, res)=>{
 
 //catch all route
 app.all("*", (req, res)=>{
-    res.writeHead(404);
-    res.end("Page not fount");
+  //the res.send() is an express function, but the below is the old way
+    // res.send(404, "Uhm, excuse me, this isn't the page you're looking for, in fact it doesn't exist!!!");
+
+  //the new way is
+    res.status(404).send("Uhm, excuse me, this isn't the page you're looking for, in fact it doesn't exist!!!")
 });
 
 app.listen(PORT, ()=> console.log(`server listening on port ${PORT}, ya filthy animal`));
