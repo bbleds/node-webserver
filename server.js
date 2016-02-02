59dcc06e18bdc9067a1b4e98c70164e7f13d1cc8
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-02 11:02:14 -0600

    setting up package .json

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 "use strict";
 
 const http = require("http");
+const { PORT } = process.env;
 
 http.createServer((req,res) => {
 
+  if(req.url === "/hello") {
+
+    const msg = '<h1>Hello World</h1>'
+    res.writeHead(200, {"Content-Type": "text/html"});
+
+    msg.split("").forEach((char, i)=>{
+
+      setTimeout(()=>{
+         res.write(char);
+      }, 100*i)
+    });
+
+
+
+  } else if (req.url === "/random"){
+     res.end("<h1>This is so random!!</h1>");
+  } else {
+    res.writeHead(403);
+    res.end("Access Denied!!!")
+  }
+
 console.log(req.method, req.url)
-}).listen(3000, ()=> console.log("server listening on port 3000, ya filthy animal"));
+}).listen(PORT, ()=> console.log(`server listening on port ${PORT}, ya filthy animal`));
 
 
 
