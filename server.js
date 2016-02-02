a84f8a32080abf351829591c8b0c5376387d4ec7
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-02 15:01:38 -0600

    added files for params with year and month

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require("express");
 const app = express();
 const { PORT } = process.env;
 
+const {outputCal} = require("./lib/monthGen.js");
+const {makeYear} = require("./lib/yearGen.js");
+
+console.log(outputCal(3,2016));
+
 
 //random route
 app.get("/random", (req, res)=>{
@@ -16,12 +21,30 @@ app.get("/hello", (req, res)=>{
     res.end(`Hello ${req.query.name}`);
 });
 
+//month route
+app.get("/cal/:year/:month", (req, res)=>{
+
+
+    let splitArray = outputCal(req.params.month,req.params.year).replace(/ /g, "&nbsp;").split("\n");
+    res.status(200).send(`<pre>${splitArray.join("<br>")}</pre>`);
+
+    res.end("Welcome to mah cal");
+});
+
+//year route
+app.get("/cal/:year", (req, res)=>{
+
+    makeYear(req.params.year);
+    res.status(200).send(`<pre>${makeYear(2016).toString()}</pre>`);
+
+    res.end("Welcome to mah cal");
+});
+
 //spaz route with Route params
 app.get("/spaz/:numOne/:numTwo", (req, res)=>{
   const getRandomArbitrary = (min, max) => {
     return Math.random() * (max - min) + min;
   }
-
     res.end(`${getRandomArbitrary(parseInt(req.params.numOne),parseInt(req.params.numTwo))}`);
 });
 
